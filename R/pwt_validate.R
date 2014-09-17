a90ab610fc401e9ea410cff7c1936f11d3a150de
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-17 16:24:07 -0700

    started validate fxn

---
+++ R/pwt_validate.R
@@ -0,0 +1,18 @@
+#' Validate an XML document
+#' 
+#' @export
+#' @param user Your PWT user name, probably your email address
+#' @param key Your PWT api key. 
+#' @param ... Further args passed to \code{httr::GET}
+#' @examples \donttest{
+#' pwt_validate()
+#' pwt_validate(config=verbose())
+#' }
+pwt_validate <- function(user, key, xml, ...){
+#   tmp <- pwt_POST(endpt="validate_document", list(xml=xml), ...)
+  message("not ready yet")
+#   meta <- get_meta(tmp)
+#   res <- xpathSApply(tmp, "//papertypes")
+#   out <- do.call(rbind, lapply(res, xmlToDataFrame, stringsAsFactors=FALSE))
+#   cbind(meta, out, stringsAsFactors = FALSE)
+}
