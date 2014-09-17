4ddfd2fe560067b4b81e6d42768649aba0e22bd2
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-17 14:59:21 -0700

    added package level man file

---
+++ R/rpwt-package.R
@@ -0,0 +1,9 @@
+#' R client for Pensoft Writing Tool.
+#' 
+#' @name rpwt-package
+#' @aliases rpwt
+#' @docType package
+#' @title R client for Pensoft Writing Tool.
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @keywords package
+NULL
